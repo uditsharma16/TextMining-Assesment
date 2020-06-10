e35ab8301d8459e174694338594fe899bd0cc018
uditsharma16 <uditsharma16@yahoo.com>
2020-06-10 19:14:49 +1200

    Checking Max topic in one file

--- Assignment-2.py
+++ Assignment-2.py
@@ -1,10 +1,18 @@
 import  xml.etree.ElementTree as ET
 xmlstring=r'C:\Users\udit sharma\Desktop\Aut\Text Mining\Assignment2BlogData\blogs\5114.male.25.indUnk.Scorpio.xml'
-
+import sys
 import time
+def remove_stop_symols(word):
+    a1=word.replace("."," ")
+    a2=a1.replace(",", " ")
+    a3=a2.replace("-", " ")
+    a4=a3.replace("!", " ")
+    a5 = a4.replace("?", " ")
+    a6 = a5.replace("\"", " ")
+    return a6
 # import xml.etree.ElementTree as ET
-parser = ET.XMLParser(encoding="utf-8")
-tree = ET.fromstring(xmlstring, parser=parser)
+# parser = ET.XMLParser(encoding="utf-8")
+# tree = ET.fromstring(xmlstring, parser=parser)
 
 # import io
 # from xml.etree import ElementTree as ET
@@ -39,65 +47,95 @@ mypath=r'C:\Users\udit sharma\Desktop\Aut\Text Mining\Assignment2BlogData\blogs'
 from os import listdir
 from os.path import isfile,join
 onlyfiles=[f for f in listdir(mypath) if isfile(join(mypath,f))]
-count=0
+
 female_no=[]
 male_no=[]
 al_20=[]
 au_20=[]
 all_no=[]
+# sys.setdefaultencoding('ANSI')
 
 
-count=0
-for file in onlyfiles:
- fileinfo=file.split('.')
- if(fileinfo[1]=="female"):
-   female_no.append(fileinfo[0])
- elif(fileinfo[1]=="male"):
-    male_no.append(fileinfo[0])
- if(int(fileinfo[2])<=20):
-    al_20.append(fileinfo[0])
- elif(int(fileinfo[2])>20):
-    au_20.append(fileinfo[0])
- all_no.append(fileinfo[0])
- a=[]
- # print(file)
- xmlstring=r'C:\Users\udit sharma\Desktop\Aut\Text Mining\Assignment2BlogData\blogs'
- joinstring='/'
- count += 1
- try:
-  with open(xmlstring+joinstring+file ,'r',encoding="ANSI") as f:
+
+# for file in onlyfiles:
+# #  fileinfo=file.split('.')
+# #  if(fileinfo[1]=="female"):
+# #    female_no.append(fileinfo[0])
+# #  elif(fileinfo[1]=="male"):
+# #     male_no.append(fileinfo[0])
+# #  if(int(fileinfo[2])<=20):
+# #     al_20.append(fileinfo[0])
+# #  elif(int(fileinfo[2])>20):
+# #     au_20.append(fileinfo[0])
+# #  all_no.append(fileinfo[0])
+# #  a=[]
+# #  # print(file)
+# #  xmlstring=r'C:\Users\udit sharma\Desktop\Aut\Text Mining\Assignment2BlogData\blogs'
+# #  joinstring='/'
+# #  count += 1
+# #
+# #  try:
+# #   with open(xmlstring+joinstring+file ,'r',encoding="ANSI") as f:
+# #     a=f.read()
+# #
+# #   f.close()
+# #   # x=a.replace("&"," ")
+# #   # cleanx = x.replace("<3", " ")
+# #   # cleanx2 = cleanx.replace("<>", " ")
+# #   # cleanx1= cleanx2.replace("< "," ")
+# #   # parser = ET.XMLParser(encoding="ANSI")
+# #   # tree = ET.fromstring(cleanx1, parser=parser)
+# #
+# #
+# #  except Exception as e:
+# #      print(count,"errornum")
+# #      print(e)
+# #      print(file)
+# #      try:
+# #       with open(xmlstring + joinstring + file, 'r', encoding="utf-8") as f:
+# #           a = f.read()
+# #
+# #       f.close()
+# #
+# #       # x = a.replace("&", " ")
+# #       # cleanx=x.replace("<3"," ")
+# #       # cleanx2 = cleanx.replace("<>", " ")
+# #       # cleanx1 = cleanx2.replace("< ", " ")
+# #       # parser = ET.XMLParser(encoding="utf-8")
+# #       # tree = ET.fromstring(cleanx1, parser=parser)
+# #      except Exception as e:
+# #
+# #          print(e)
+# #          print(file)
+# #          exit()
+xmlstring=r'C:\Users\udit sharma\Desktop\Aut\Text Mining\Assignment2BlogData\blogs'
+joinstring='/'
+  # count += 1
+
+file=onlyfiles[0]
+with open(xmlstring+joinstring+file ,'r',encoding="ANSI") as f:
     a=f.read()
 
-  f.close()
-  x=a.replace("&"," ")
-  cleanx = x.replace("<3", " ")
-  cleanx2 = cleanx.replace("<>", " ")
-  cleanx1= cleanx2.replace("< "," ")
-  parser = ET.XMLParser(encoding="ANSI")
-  tree = ET.fromstring(cleanx1, parser=parser)
+f.close()
+max_occur=0
+counts=dict()
+abc=remove_stop_symols(a)
+abx=abc.split()
+for i,word in enumerate(list(set(abx))):
+    # print(word)
+    if(word in counts):
+        counts[word]+=1
+    else:
+        counts[word]=1
+top=[]
+for word in counts:
+    if(counts[word]>max_occur):
+        max_occur=counts[word]
+        top.append(word)
 
+print(counts[top[len(top)-1]])
+print("Hello")
 
- except Exception as e:
-     print(count)
-     print(e)
-     print(file)
-     try:
-      with open(xmlstring + joinstring + file, 'r', encoding="utf-8") as f:
-          a = f.read()
-
-      f.close()
-      x = a.replace("&", " ")
-      cleanx=x.replace("<3"," ")
-      cleanx2 = cleanx.replace("<>", " ")
-      cleanx1 = cleanx2.replace("< ", " ")
-      parser = ET.XMLParser(encoding="utf-8")
-      tree = ET.fromstring(cleanx1, parser=parser)
-     except Exception as e:
-         print(e)
-         print(file)
-         exit()
-
-print(count)
 end=time.time()
 
 
