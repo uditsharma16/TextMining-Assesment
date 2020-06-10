c3e1a3fa484975aa4234d2121d6f2ab8a70fd42a
uditsharma16 <uditsharma16@yahoo.com>
2020-06-10 18:28:16 +1200

    adding exception handeling for different formats

--- Assignment-2.py
+++ Assignment-2.py
@@ -1,8 +1,11 @@
-
+import  xml.etree.ElementTree as ET
 xmlstring=r'C:\Users\udit sharma\Desktop\Aut\Text Mining\Assignment2BlogData\blogs\5114.male.25.indUnk.Scorpio.xml'
+
+import time
 # import xml.etree.ElementTree as ET
-# parser = ET.XMLParser(encoding="utf-8")
-# tree = ET.fromstring(xmlstring, parser=parser)
+parser = ET.XMLParser(encoding="utf-8")
+tree = ET.fromstring(xmlstring, parser=parser)
+
 # import io
 # from xml.etree import ElementTree as ET
 #
@@ -23,6 +26,13 @@ xmlstring=r'C:\Users\udit sharma\Desktop\Aut\Text Mining\Assignment2BlogData\blo
 # tree = ET.parse(r'C:\Users\udit sharma\Desktop\Aut\Text Mining\Assignment2BlogData\blogs\5114.male.25.indUnk.Scorpio.xml')
 # root = tree.getroot()
 # print(root)
+# parser = etree.XMLParser(ns_clean=True)
+# tree   = etree.parse(io.StringIO(tree), parser)
+# print(etree.tostring(tree.getroot()))
+# import xml.etree.ElementTree as ET
+# parser = ET.XMLParser(encoding="utf-8")
+# tree = ET.fromstring(xmlstring, parser=parser)
+start=time.time()
 from lxml import etree
 import io
 mypath=r'C:\Users\udit sharma\Desktop\Aut\Text Mining\Assignment2BlogData\blogs'
@@ -37,7 +47,7 @@ au_20=[]
 all_no=[]
 
 
-
+count=0
 for file in onlyfiles:
  fileinfo=file.split('.')
  if(fileinfo[1]=="female"):
@@ -49,11 +59,46 @@ for file in onlyfiles:
  elif(int(fileinfo[2])>20):
     au_20.append(fileinfo[0])
  all_no.append(fileinfo[0])
-print(len(female_no))
-print(len(male_no))
-# for tree in files:
-#  print(counttree)
+ a=[]
+ # print(file)
+ xmlstring=r'C:\Users\udit sharma\Desktop\Aut\Text Mining\Assignment2BlogData\blogs'
+ joinstring='/'
+ count += 1
+ try:
+  with open(xmlstring+joinstring+file ,'r',encoding="ANSI") as f:
+    a=f.read()
 
-# parser = etree.XMLParser(ns_clean=True)
-# tree   = etree.parse(io.StringIO(tree), parser)
-# print(etree.tostring(tree.getroot()))
+  f.close()
+  x=a.replace("&"," ")
+  cleanx = x.replace("<3", " ")
+  cleanx2 = cleanx.replace("<>", " ")
+  cleanx1= cleanx2.replace("< "," ")
+  parser = ET.XMLParser(encoding="ANSI")
+  tree = ET.fromstring(cleanx1, parser=parser)
+
+
+ except Exception as e:
+     print(count)
+     print(e)
+     print(file)
+     try:
+      with open(xmlstring + joinstring + file, 'r', encoding="utf-8") as f:
+          a = f.read()
+
+      f.close()
+      x = a.replace("&", " ")
+      cleanx=x.replace("<3"," ")
+      cleanx2 = cleanx.replace("<>", " ")
+      cleanx1 = cleanx2.replace("< ", " ")
+      parser = ET.XMLParser(encoding="utf-8")
+      tree = ET.fromstring(cleanx1, parser=parser)
+     except Exception as e:
+         print(e)
+         print(file)
+         exit()
+
+print(count)
+end=time.time()
+
+
+
